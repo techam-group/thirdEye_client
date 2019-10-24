7f96f65b87167f4d319650a9c1ab9339d6ac11ea
Favour George <phavorsparks@gmail.com>
2019-10-24 21:55:18 +0100

    added the fligth cover styles

--- src/styles/landing/index.styles.js
+++ src/styles/landing/index.styles.js
@@ -14,7 +14,8 @@ export const useStyles = makeStyles(theme => ({
     backgroundSize: 'cover',
     display: 'flex',
     alignItems: 'center',
-    padding: theme.spacing(0, 8)
+    padding: theme.spacing(0, 8),
+    width: '100%'
   },
   sme__cta: {
     width: '35%',
@@ -41,5 +42,55 @@ export const useStyles = makeStyles(theme => ({
     '&:hover': {
       backgroundColor: '#CE2D2D',
     }
+  },
+  sme__navs: {
+    width: '40rem',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: '-74px',
+    height: '6.5rem',
+    padding: theme.spacing(0, 2),
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+
+    '& li': {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column',
+      textTransform: 'uppercase',
+
+      '&:nth-child(1)': {
+        color: '#CE2D2D',
+
+        '& .MuiIconButton-root': {
+          color: '#ff5d79d6'
+        }
+      },
+
+      '&:nth-child(2)': {
+        color: '#0581b9',
+
+        '& .MuiIconButton-root': {
+          color: '#00b0ffb0',
+        }
+      },
+
+      '&:nth-child(3)': {
+        color: '#ec0ebf',
+
+        '& .MuiIconButton-root': {
+          color: '#ec0ebfab',
+        }
+      }
+    }
+  },
+  sme__flightCover: {
+    position: 'relative',
+    display: 'flex',
+    justifyContent: 'center',
+    marginBottom: theme.spacing(17)
   }
 }))
